a95879e585f8d937cef9710cf8fbcc1998d941ff
Kevin Pyle <lxc.8eaf7cd8e5128d8191fe@spamgourmet.com>
2011-10-02 18:01:33 -0500

    conf: remove forward declarations
    Move struct config to the end of the file.  Rely on the definitions of
    the static functions to declare them.

--- src/lxc/confile.c
+++ src/lxc/confile.c
@@ -48,33 +48,6 @@
 
 lxc_log_define(lxc_confile, lxc);
 
-static int config_personality(const char *, char *, struct lxc_conf *);
-static int config_pts(const char *, char *, struct lxc_conf *);
-static int config_tty(const char *, char *, struct lxc_conf *);
-static int config_ttydir(const char *, char *, struct lxc_conf *);
-static int config_cgroup(const char *, char *, struct lxc_conf *);
-static int config_mount(const char *, char *, struct lxc_conf *);
-static int config_rootfs(const char *, char *, struct lxc_conf *);
-static int config_rootfs_mount(const char *, char *, struct lxc_conf *);
-static int config_pivotdir(const char *, char *, struct lxc_conf *);
-static int config_utsname(const char *, char *, struct lxc_conf *);
-static int config_network_type(const char *, char *, struct lxc_conf *);
-static int config_network_flags(const char *, char *, struct lxc_conf *);
-static int config_network_link(const char *, char *, struct lxc_conf *);
-static int config_network_name(const char *, char *, struct lxc_conf *);
-static int config_network_veth_pair(const char *, char *, struct lxc_conf *);
-static int config_network_macvlan_mode(const char *, char *, struct lxc_conf *);
-static int config_network_hwaddr(const char *, char *, struct lxc_conf *);
-static int config_network_vlan_id(const char *, char *, struct lxc_conf *);
-static int config_network_mtu(const char *, char *, struct lxc_conf *);
-static int config_network_ipv4(const char *, char *, struct lxc_conf *);
-static int config_network_ipv4_gateway(const char *, char *, struct lxc_conf *);
-static int config_network_script(const char *, char *, struct lxc_conf *);
-static int config_network_ipv6(const char *, char *, struct lxc_conf *);
-static int config_network_ipv6_gateway(const char *, char *, struct lxc_conf *);
-static int config_cap_drop(const char *, char *, struct lxc_conf *);
-static int config_console(const char *, char *, struct lxc_conf *);
-
 typedef int (*config_cb)(const char *, char *, struct lxc_conf *);
 
 struct config {
@@ -82,48 +55,7 @@ struct config {
 	config_cb cb;
 };
 
-static const struct config config[] = {
-
-	{ "lxc.arch",                 config_personality          },
-	{ "lxc.pts",                  config_pts                  },
-	{ "lxc.tty",                  config_tty                  },
-	{ "lxc.devttydir",            config_ttydir               },
-	{ "lxc.cgroup",               config_cgroup               },
-	{ "lxc.mount",                config_mount                },
-	{ "lxc.rootfs.mount",         config_rootfs_mount         },
-	{ "lxc.rootfs",               config_rootfs               },
-	{ "lxc.pivotdir",             config_pivotdir             },
-	{ "lxc.utsname",              config_utsname              },
-	{ "lxc.network.type",         config_network_type         },
-	{ "lxc.network.flags",        config_network_flags        },
-	{ "lxc.network.link",         config_network_link         },
-	{ "lxc.network.name",         config_network_name         },
-	{ "lxc.network.macvlan.mode", config_network_macvlan_mode },
-	{ "lxc.network.veth.pair",    config_network_veth_pair    },
-	{ "lxc.network.script.up",    config_network_script       },
-	{ "lxc.network.hwaddr",       config_network_hwaddr       },
-	{ "lxc.network.mtu",          config_network_mtu          },
-	{ "lxc.network.vlan.id",      config_network_vlan_id      },
-	{ "lxc.network.ipv4.gateway", config_network_ipv4_gateway },
-	{ "lxc.network.ipv4",         config_network_ipv4         },
-	{ "lxc.network.ipv6.gateway", config_network_ipv6_gateway },
-	{ "lxc.network.ipv6",         config_network_ipv6         },
-	{ "lxc.cap.drop",             config_cap_drop             },
-	{ "lxc.console",              config_console              },
-};
-
-static const size_t config_size = sizeof(config)/sizeof(struct config);
-
-static const struct config *getconfig(const char *key)
-{
-	int i;
-
-	for (i = 0; i < config_size; i++)
-		if (!strncmp(config[i].name, key,
-			     strlen(config[i].name)))
-			return &config[i];
-	return NULL;
-}
+static const struct config *getconfig(const char *key);
 
 static const char *get_netdev_typename(const enum lxc_network_type_t type)
 {
@@ -1071,4 +1003,46 @@ signed long lxc_config_parse_arch(const char *arch)
 	}
 
 	return -1;
+}
+
+static const struct config config[] = {
+	{ "lxc.arch",                 config_personality          },
+	{ "lxc.pts",                  config_pts                  },
+	{ "lxc.tty",                  config_tty                  },
+	{ "lxc.devttydir",            config_ttydir               },
+	{ "lxc.cgroup",               config_cgroup               },
+	{ "lxc.mount",                config_mount                },
+	{ "lxc.rootfs.mount",         config_rootfs_mount         },
+	{ "lxc.rootfs",               config_rootfs               },
+	{ "lxc.pivotdir",             config_pivotdir             },
+	{ "lxc.utsname",              config_utsname              },
+	{ "lxc.network.type",         config_network_type         },
+	{ "lxc.network.flags",        config_network_flags        },
+	{ "lxc.network.link",         config_network_link         },
+	{ "lxc.network.name",         config_network_name         },
+	{ "lxc.network.macvlan.mode", config_network_macvlan_mode },
+	{ "lxc.network.veth.pair",    config_network_veth_pair    },
+	{ "lxc.network.script.up",    config_network_script       },
+	{ "lxc.network.hwaddr",       config_network_hwaddr       },
+	{ "lxc.network.mtu",          config_network_mtu          },
+	{ "lxc.network.vlan.id",      config_network_vlan_id      },
+	{ "lxc.network.ipv4.gateway", config_network_ipv4_gateway },
+	{ "lxc.network.ipv4",         config_network_ipv4         },
+	{ "lxc.network.ipv6.gateway", config_network_ipv6_gateway },
+	{ "lxc.network.ipv6",         config_network_ipv6         },
+	{ "lxc.cap.drop",             config_cap_drop             },
+	{ "lxc.console",              config_console              },
+};
+
+static const size_t config_size = sizeof(config)/sizeof(struct config);
+
+static const struct config *getconfig(const char *key)
+{
+	const size_t config_size = sizeof(config) / sizeof(config[0]);
+	const struct config *const end = config + config_size;
+	const struct config *c = config;
+	for (; c != end; ++ c)
+		if (!strncmp(c->name, key, strlen(c->name)))
+			return c;
+	return NULL;
 }
